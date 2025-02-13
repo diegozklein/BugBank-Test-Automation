19f77119b849f4df29977b609e4e06bca638a9bc
diegozklein <diego.klein.work@gmail.com>
2025-02-13 17:54:39 -0300

    Testes de cadastro

--- cypress/e2e/home_page.cy.js
+++ cypress/e2e/home_page.cy.js
@@ -32,3 +32,27 @@ describe('Testes Login', () => {
         cy.get('#modalText').should('contain.text', 'Usuário ou senha inválido');
   })
 })
+
+describe('Testes Cadastro', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get('.ihdmxA').click();
+  })
+
+  it('Cadastro - Sucesso', () => {
+    cy.get(':nth-child(2) > .input__default').type('teste@gmail.com', {force: true});
+    cy.get(':nth-child(3) > .input__default').type('John Doe', {force: true});
+    cy.get(':nth-child(4) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('123456789', {force: true});
+    cy.get(':nth-child(5) > .style__ContainerFieldInput-sc-s3e9ea-0 > .input__default').type('123456789', {force: true});
+    cy.get('.styles__ContainerFormRegister-sc-7fhc7g-0 > .style__ContainerButton-sc-1wsixal-0').click({force: true});
+    cy.get('#modalText').should('contain.text', 'foi criada com sucesso');
+  })
+
+  it('Cadastro - Email com formato inválido ', () => {
+    cy.get(':nth-child(2) > .input__default').type('teste@gmail', {force: true});
+    cy.get('.kOeYBn > .input__warging').should('have.text', 'Formato inválido');
+  })
+
+  
+})
